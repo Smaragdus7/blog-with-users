9d1c04f4077436fc1162246af974d37c2d8e6083
Smaragdus7 <smar.leeft@gmail.com>
2022-08-25 13:56:13 -0500

    SQLite to Postgre

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
